7a219db9a9dcedca12f05d61c4c9accd1ee49c71
Emanuel Fortes <emanuelbf19@gmail.com>
2023-06-28 15:33:38 -0300

    resolver o problema da sessao  nossos clientes

--- js/empresas-slide.js
+++ js/empresas-slide.js
@@ -3,27 +3,23 @@ let swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     loop: true,
     slidesPerView: 4,
-    spaceBetween: 50,
+    spaceBetween:60,
     grabCursor: true,
     centeredSlides: true,
     coverflowEffect: {
       rotate: 0,
       stretch: 0,
-      depth: 100,
-      modifier: 1,
+      depth: 125,
+      modifier: 2,
       slideShadows: false,
     },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-    },
     pagination: {
       el: ".swiper-pagination",
-      type: "bullets",
+      // type: "bullets",
     },
     // autoplay: {
     //   delay: 2000,
